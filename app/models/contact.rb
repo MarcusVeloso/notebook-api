2781a9740ec6c998cff1eccd454cdd56fb768fa7
Marcus Veloso <desenvolvedoroficial@gmail.com>
2017-12-13 23:28:21 +0000

    Install gem I18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,16 @@
 class Contact < ApplicationRecord
 	belongs_to :kind
 
+	def birthdate_br
+		I18n.l(self.birthdate) unless self.birthdate.blank?					
+	end
+
+	def to_br
+		 {name: self.name,
+		  email: self.email,
+		  birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?) }
+	end
+
 	# def author
 	# 	"Marcus Veloso"
 	# end
@@ -24,4 +34,13 @@ class Contact < ApplicationRecord
 	# 		include: :kind
 	# 		)
 	# end
+
+	# def hello
+	# 	I18n.t('hello')		
+	# end
+
+	# def i18n
+	# 	I18n.default_locale		
+	# end
+
 end
